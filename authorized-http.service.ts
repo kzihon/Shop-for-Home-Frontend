514505d166252bea78b37b6bd51bf05f226c7f62
BBullettt <159592906+BBullettt@users.noreply.github.com>
2024-03-06 11:03:24 -0800

    Add files via upload

---
+++ authorized-http.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core'
+import { env } from '../../env'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthorizedHttpService {
+  private TOKEN = env.TEMP_TOKEN
+  private BASE_SERVER_URI = env.SERVER_URI
+
+  constructor (private http: HttpClient) {}
+
+  public get (url: string, options?: any) {
+    return this.http.get(this.BASE_SERVER_URI + url, {
+      headers: this.authorizedHeader,
+      ...options
+    })
+  }
+
+  public post (url: string, data: any, options?: any) {
+    return this.http.post(this.BASE_SERVER_URI + url, data, {
+      headers: this.authorizedHeader,
+      ...options
+    })
+  }
+
+  public put (url: string, data: any, options?: any) {
+    return this.http.put(this.BASE_SERVER_URI + url, data, {
+      headers: this.authorizedHeader,
+      ...options
+    })
+  }
+
+  public delete (url: string, options?: any) {
+    return this.http.delete(this.BASE_SERVER_URI + url, {
+      headers: this.authorizedHeader,
+      ...options
+    })
+  }
+
+  private get authorizedHeader (): HttpHeaders {
+    let authHeaders: HttpHeaders = new HttpHeaders()
+
+    return authHeaders.set(
+      'Authorization',
+      `Bearer ${/* this.authlocalStorageService.token  */ this.TOKEN}`
+    )
+  }
+}
